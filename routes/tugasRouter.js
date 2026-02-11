0854066cf04c3dce84fedadf52257a0f8359270c
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-02-11 10:25:26 +0700

    feat: api PR

---
+++ routes/tugasRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const tugasController = require('../controllers/tugasController');
+
+const upload = multer({ 
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 }
+});
+
+router.post('/', upload.single('file'), tugasController.createTugas);
+router.get('/', tugasController.getAllTugas);
+router.get('/:id', tugasController.getTugasById);
+router.put('/:id', tugasController.updateTugas);
+router.delete('/:id', tugasController.deleteTugas);
+
+module.exports = router;
