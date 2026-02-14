a71d538b85f7b0e67461898cae948eb6aa5240e1
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-02-14 20:06:28 +0700

    fix api

--- models/orangTua.js
+++ models/orangTua.js
@@ -5,9 +5,9 @@ const Parent = sequelize.define('Parent', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   schoolId: { type: DataTypes.INTEGER, allowNull: false },
   name: { type: DataTypes.STRING, allowNull: false },
-  gender: { type: DataTypes.ENUM('Laki-laki', 'Perempuan'), allowNull: false },
-  relationStatus: { type: DataTypes.ENUM('Ayah', 'Ibu'), allowNull: false },
-  type: { type: DataTypes.ENUM('Kandung', 'Tiri'), defaultValue: 'Kandung' },
+  gender: { type: DataTypes.STRING, allowNull: false },
+  relationStatus: { type: DataTypes.STRING, allowNull: false },
+  type: { type: DataTypes.STRING, defaultValue: 'Kandung' },
   phoneNumber: { type: DataTypes.STRING, allowNull: false, unique: true },
   isActive: { type: DataTypes.BOOLEAN, defaultValue: true }
 }, {
