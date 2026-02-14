ac117a26ada180a45c671da58f9d40e5fd836264
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-02-14 09:50:46 +0700

    fix api

--- routes/settingRatingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const settingRatingController = require('../controllers/settingRatingController');
-
-router.get('/', settingRatingController.getSettings);
-router.post('/', settingRatingController.updateSettings);
-
-module.exports = router;
