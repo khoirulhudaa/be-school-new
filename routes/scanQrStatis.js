040c53d556a9b121ec1ca8ca61a575e183314df3
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-03-22 11:06:12 +0700

    login with qr NEW

--- routes/scanQrStatis.js
+++ routes/scanQrStatis.js
@@ -5,6 +5,6 @@ const scanQrMiddleware = require('../middlewares/scanQrStatis');
 
 router.post('/', scanQrMiddleware, scanQrController.scanSelf);
 router.post('/login-qr', scanQrController.loginWithQR);   // atau siswaController.loginWithQR
-router.post('/login-qr-new', scanQrController.loginWithQRNew);   // atau siswaController.loginWithQR
+router.post('/login-qr-new', scanQrMiddleware, scanQrController.loginWithQRNew);   // atau siswaController.loginWithQR
 
 module.exports = router;
