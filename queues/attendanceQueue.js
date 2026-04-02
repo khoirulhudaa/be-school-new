2b03020f7efb1d04579a71df4dd3e41d5dd9406b
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-04-02 22:30:06 +0700

    fix

--- queues/attendanceQueue.js
+++ queues/attendanceQueue.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
+
 const { Queue } = require('bullmq');
 const Redis = require('ioredis');
 
-const connection = new Redis({
-  host: '127.0.0.1',
-  port: 6379,
+const connection = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null
 });
 
