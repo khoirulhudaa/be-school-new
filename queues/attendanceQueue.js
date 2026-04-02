a8c6a5fefc824549d09b74f4d622f018aba65f65
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-04-02 22:17:07 +0700

    worker

--- queues/attendanceQueue.js
+++ queues/attendanceQueue.js
@@ -3,7 +3,8 @@ const Redis = require('ioredis');
 
 const connection = new Redis({
   host: '127.0.0.1',
-  port: 6379
+  port: 6379,
+  maxRetriesPerRequest: null
 });
 
 const attendanceQueue = new Queue('attendance-queue', {
